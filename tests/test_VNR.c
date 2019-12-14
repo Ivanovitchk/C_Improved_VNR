565fc457e9ae80659903b5d908bb8948863fdb9c
Ivanovitch_k <ivanovitchk@gmail.com>
2019-12-14 16:14:37 +0100

    add test syntax file

---
+++ tests/test_VNR.c
@@ -0,0 +1,17 @@
+au8FS_data[u8PROGR_OK_IDX] TRUE
+vidInit bTest btest; btest bTest beTest psb2222 u16test au8 u8test
+MOD_u8Test MOD_u8test MOD_bTest  MOD_btest MOD_vidInit vidInit pabTa
+GLB_u8Test GLB_au8Test GLB_vidInit
+boolean s_boolean uint8 sint8 uint32
+
+uint8_t uint8  tstrA tudtA
+
+  kpabTest kabTest cbTest bTest cpau8Test cu16Test
+
+__interrupt Pragma ghs interrupt isr
+{ MOD_vidInit() vidInit() TEST(); TEST; } vidInit ();
+void const MOD_vidGLB_Init (uint8 GLB_abbBB );
+
+printf();
+
+// WTF  /*  FIXME TODO */
